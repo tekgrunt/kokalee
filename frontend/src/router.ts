9e2c44461bddcbc25cbd1e5d5409a1dc8cd15899
Emily Marigold Klassen <forivall@gmail.com>
2017-12-31 17:50:01 -0800

    refactor: replace old about component with single vue file

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -3,16 +3,16 @@ import VueRouter, {Location, Route, RouteConfig} from 'vue-router';
 import {makeHot, reload} from './util/hot-reload';
 
 const homeComponent = () => import('./components/home').then(({HomeComponent}) => HomeComponent);
-const aboutComponent = () => import('./components/about_.vue').then(({default: AboutComponent}) => AboutComponent);
+const aboutComponent = () => import('./components/about.vue').then(({default: AboutComponent}) => AboutComponent);
 const listComponent = () => import('./components/list').then(({ListComponent}) => ListComponent);
 const sidemenuComponent = () => import('./components/sidemenu').then(({SideMenuComponent}) => SideMenuComponent);
 // const homeComponent = () => import(/* webpackChunkName: 'home' */'./components/home').then(({HomeComponent}) => HomeComponent);
-// const aboutComponent = () => import(/* webpackChunkName: 'about' */'./components/about_.vue').then(({AboutComponent}) => AboutComponent);
+// const aboutComponent = () => import(/* webpackChunkName: 'about' */'./components/about.vue').then(({AboutComponent}) => AboutComponent);
 // const listComponent = () => import(/* webpackChunkName: 'list' */'./components/list').then(({ListComponent}) => ListComponent);
 
 if (process.env.ENV === 'development' && module.hot) {
   const homeModuleId = './components/home';
-  const aboutModuleId = './components/about_.vue';
+  const aboutModuleId = './components/about.vue';
   const listModuleId = './components/list';
   const sidemenuModuleId = './components/sidemenu';
 
@@ -22,7 +22,7 @@ if (process.env.ENV === 'development' && module.hot) {
     module.hot.accept('./components/home', () => reload(homeModuleId, (<any>require('./components/home')).HomeComponent)));
 
   makeHot(aboutModuleId, aboutComponent,
-    module.hot.accept('./components/about_.vue', () => reload(aboutModuleId, (<any>require('./components/about_.vue')).default)));
+    module.hot.accept('./components/about.vue', () => reload(aboutModuleId, (<any>require('./components/about.vue')).default)));
 
   makeHot(listModuleId, listComponent,
     module.hot.accept('./components/list', () => reload(listModuleId, (<any>require('./components/list')).ListComponent)));
