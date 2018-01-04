0cc5aba45351233fa5ab4312e24842fefeaf9c64
Emily Marigold Klassen <forivall@gmail.com>
2018-01-04 01:03:56 -0800

    try to get login component to work i guess

--- frontend/src/components/login/box.vue
+++ frontend/src/components/login/box.vue
@@ -1,5 +1,5 @@
 <template>
-  <ui :login="login"></ui>
+  <ui :login="login" :signup="signup" :logout="logout" :user="user"></ui>
 </template>
 
 <script lang="ts">
@@ -7,16 +7,43 @@
 import Vue, {ComponentOptions} from 'vue'
 import Component from 'vue-class-component';
 import Ui from './ui.vue';
+import hoodie from '../../util/hoodie'
 // import {Logger} from '../util/log';
+
+export interface User {
+  username: string
+  password: string
+}
 
 @Component({
   components: {
     Ui
-  }
+  },
+  data: () => ({
+    user: null
+  })
 } as ComponentOptions<Vue>)
 export default class LoginContainer extends Vue {
-  login(credentials: {username: string, password: string}) {
-    console.log('logging in...')
+  constructor() {
+    super();
+    console.log('get user')
+    hoodie.account.get().then((user) => {
+      console.log('got user', user)
+      if (user.session) {
+        this.$data.user = user
+      }
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+  login(credentials: User) {
+    return hoodie.account.signIn(credentials)
+  }
+  signup(credentials: User) {
+    return hoodie.account.signUp(credentials)
+  }
+  logout() {
+    return hoodie.account.signOut()
   }
 }
 </script>
--- frontend/src/components/login/ui.vue
+++ frontend/src/components/login/ui.vue
@@ -1,8 +1,14 @@
 <template>
-  <form @submit.prevent="_login()">
-    <input type="text" name="username" v-model="username">
-    <input type="password" name="password" v-model="password">
+  <form v-if="user">
+    {{ user.username }} is currently logged in.
+    <button type="submit">Logout</button>
+  </form>
+  <form v-else @submit.prevent="_login()">
+    <div>{{error}}</div>
+    <input type="text" name="username" v-model="credentials.username">
+    <input type="password" name="password" v-model="credentials.password">
     <button type="submit">Login</button>
+    <button>Create Account</button>
   </form>
 </template>
 
@@ -12,25 +18,50 @@ import Vue from 'vue'
 import Component from 'vue-class-component';
 
 @Component({
-  props: [
-    'login'
-  ],
-  data: () => ({
-    username: '',
-    password: ''
-  })
+  props: {
+    login: {type: Function},
+    signup: {type: Function},
+    logout: {type: Function},
+    user: {default: null}
+  },
+  data: function (this: LoginUi) {
+    return {
+      credentials: {
+        username: '',
+        password: ''
+      },
+      error: null
+    };
+  }
 })
 export default class LoginUi extends Vue {
+  user: {id: string, session: string, username: string}
+  _onError = (err) => {
+    this.$data.error = err;
+  }
   _login() {
-    if (typeof this.$props.login === 'function') {
-      Promise.resolve(this.$props.login(this.$data))
-      .then(() => {
-        console.log('TODO: show success ui');
-      }).catch((err) => {
-        console.log('TODO: show err ui');
-      })
-      ;
-    }
+    if (!(typeof this.$props.login === 'function')) return;
+
+    Promise.resolve(this.$props.login(this.$data.credentials))
+    .then((user) => {
+      console.log('TODO: show success ui or welcome message, etc.');
+    }).catch(this._onError);
+  }
+  _logout() {
+    if (!(typeof this.$props.login === 'function')) return;
+
+    Promise.resolve(this.$props.logout())
+    .then((user) => {
+      console.log('TODO: show success ui or goodbye message, etc.');
+    }).catch(this._onError);
+  }
+  _signup() {
+    if (!(typeof this.$props.login === 'function')) return;
+
+    Promise.resolve(this.$props.signup(this.$data.credentials))
+    .then((user) => {
+      console.log('TODO: show success ui or welcome message, etc.');
+    }).catch(this._onError);
   }
 }
 
