a26f75c21b8dbe51ae386bfde24e66b2f537b70b
rachel-joy <racheljvoth@gmail.com>
2018-01-31 14:29:38 -0800

    dynamic import for info component

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -17,8 +17,7 @@ const sidemenuComponent = () => import('./components/sidemenu')
 const login = () => import('./components/login.vue')
 .then(({default: c}) => c);
 const info = () => import('./components/info.vue')
-// .then(({InfoComponent}) => InfoComponent);
-
+.then(({default: InfoComponent}) => InfoComponent);
 
 import './sass/main.scss';
 import {AppComponent} from './util/types';
